f89977d4f41ab43058664a640dca39b977066839
marcelscruz <marcelcruz@live.com>
2020-02-27 17:04:00 -0300

    Add Knobs

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,26 +1,43 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
 
 import Task from "./Task";
 
-export const task = {
+export default {
+  component: Task,
+  title: "Task",
+  decorators: [withKnobs],
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/
+};
+
+export const taskData = {
   id: "1",
   title: "Test Task",
   state: "TASK_INBOX",
   updatedAt: new Date(2018, 0, 1, 9, 0)
 };
 
-export const actions = {
+export const actionsData = {
   onPinTask: action("onPinTask"),
   onArchiveTask: action("onArchiveTask")
 };
 
-storiesOf("Task", module)
-  .add("default", () => <Task task={task} {...actions} />)
-  .add("pinned", () => (
-    <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
-  ))
-  .add("archived", () => (
-    <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
-  ));
+export const Default = () => {
+  return <Task task={object("task", { ...taskData })} {...actionsData} />;
+};
+
+export const Pinned = () => (
+  <Task task={{ ...taskData, state: "TASK_PINNED" }} {...actionsData} />
+);
+
+export const Archived = () => (
+  <Task task={{ ...taskData, state: "TASK_ARCHIVED" }} {...actionsData} />
+);
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = () => (
+  <Task task={{ ...taskData, title: longTitleString }} {...actionsData} />
+);
